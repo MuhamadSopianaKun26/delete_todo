fccc217348fb3524e2de73f529cd60e88bc68265
MuhamadSopianaKun26 <argiansyah264@gmail.com>
2025-02-18 14:30:37 +0700

    nambah ReadFile

--- ToDoList.py
+++ ToDoList.py
@@ -1,18 +1,43 @@
 import msvcrt
 import os
 
+data = [
+{"id":1, "todo":"Makan", "jam":"10.00"}
+
+]
 
 def DeleteToDo(x):
-    with open('DataToDo.txt', 'r') as file:
+    with open('Data.txt', 'r') as file:
         lines = file.readlines()
 
-    with open('DataToDo.txt', 'w') as file:
+    with open('Data.txt', 'w') as file:
         for i, line in enumerate(lines):
-            ToDo = line.strip().split(',')
+            line.strip().split(',')
             if i != x - 1: 
                 file.write(line)
 
-x = int(input("Masukkan angka (ID yang akan dihapus): "))
-DeleteToDo(x)
+def Readfile():
+    Data = {}
+    path = "Data.txt"
+    cek_file = os.path.isfile(path)
+    
+    if not cek_file:
+        with open(path, 'w') as file:
+            file.write("")
+    else:
+        with open(path, 'r') as file:
 
-print(f"Data dengan ID {x} telah dihapus.")
+            for line in file:
+                Isi = line.strip().split(",")
+                if len(Isi) == 3:
+                    key, do, jam = Isi
+                    Data[key] = {"Do": do, "Jam": jam}
+
+            file.close()
+
+            for key, value in Data.items():
+                print(f"{key}. Do: {value['Do']}, Jam: {value['Jam']}")
+    
+
+Readfile()
+DeleteToDo(4)
